6d4ad90ebd169e5f0e215b6144d87258d31c737c
siimav <siim.aaver@gmail.com>
2023-10-20 07:49:35 +0300

    Greatly increase asteroid load and unpack distances (#292)

---
+++ Source/HarmonyPatcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RealSolarSystem
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class HarmonyPatcher : MonoBehaviour
+    {
+        internal void Start()
+        {
+            var harmony = new HarmonyLib.Harmony("RSS.HarmonyPatcher");
+            harmony.PatchAll();
+        }
+    }
+}
